f8fb54e772ef0839705f89af869ac49a9030c3e4
Peter Rekdal Khan-Sunde <peter.sunde@gmail.com>
2026-09-04 09:29:48 +0200

    feat(remote): add secure worker image contract

---
+++ containers/remote-worker/session.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -eu
+
+if [ "$#" -eq 0 ]; then
+    printf '%s\n' "usage: horizon-agent-session <command> [argument ...]" >&2
+    exit 64
+fi
+
+export PATH="/usr/local/cargo/bin:${PATH}"
+
+if [ -f /run/horizon/github-token ]; then
+    GITHUB_TOKEN=$(cat /run/horizon/github-token)
+    export GITHUB_TOKEN
+    GH_TOKEN=${GITHUB_TOKEN}
+    export GH_TOKEN
+    gh auth setup-git
+    git config --global url.https://github.com/.insteadOf git@github.com:
+    git config --global --add url.https://github.com/.insteadOf ssh://git@github.com/
+fi
+
+export HORIZON=1
+exec "$@"
